1fabc8ba827616ed500e0896d3c89bf817cf49fb
Chathuranga <UCHATNU@PEROOT.COM>
2016-03-02 11:36:26 +0530

    initial commit

---
+++ src/components/Comment.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+
+export default class Comment extends Component{
+
+	constructor(props){
+		super(props);		 
+	}
+
+	render(){
+		return(
+			<div id="comment-wrapper">
+				
+			</div>
+		);
+	}
+}
+
+
+
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Comment from './components/Comment';
+
+render(
+	<Comment/>,
+	document.getElementById('container')
+);
+
+
+
+
